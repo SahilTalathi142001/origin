66ff02936c61e6a0057fae4825d42e67b5c5254c
SahilTalathi142001 <80190527+SahilTalathi142001@users.noreply.github.com>
2024-07-04 20:40:12 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
